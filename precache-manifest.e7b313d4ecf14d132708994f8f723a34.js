60ed93bc26826da3244d776d22ce9e558f536cc3
ntaskumar <145751168+ntaskumar@users.noreply.github.com>
2024-07-19 09:43:24 +0530

    Add files via upload

---
+++ precache-manifest.e7b313d4ecf14d132708994f8f723a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e86fe5f76298d801b8b7ab5e5409fc6",
+    "url": "/diaganalmtest/index.html"
+  },
+  {
+    "revision": "4060aa26e2829c168d5a",
+    "url": "/diaganalmtest/static/css/main.3b4f3811.chunk.css"
+  },
+  {
+    "revision": "fcc7b00908aeb5dfa96a",
+    "url": "/diaganalmtest/static/js/2.573f90b5.chunk.js"
+  },
+  {
+    "revision": "4060aa26e2829c168d5a",
+    "url": "/diaganalmtest/static/js/main.e1db8423.chunk.js"
+  },
+  {
+    "revision": "f86bbab3e1fbb8b26c66",
+    "url": "/diaganalmtest/static/js/runtime~main.ae87c24e.js"
+  }
+]);
